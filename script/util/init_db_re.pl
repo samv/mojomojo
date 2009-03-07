17b1455bc108d01e8eb5e2653ca20133f67879b2
mateu x hunter <hunter@missoula.org>
2009-03-07 09:01:54 -0500

    Find specific node to move.

--- script/util/init_db_re.pl
+++ script/util/init_db_re.pl
@@ -6,18 +6,24 @@ use lib "$FindBin::Bin/../../lib";
 use MojoMojo::Schema;
 use base 'Exporter';
 our @EXPORT =
-  qw( $schema $preference_rs $page_rs @root_lower_family $root_page $number_of_family_members );
+  qw( $schema $preference_rs $page_rs @root_lower_family $root_page
+  $number_of_family_members $howtos_node);
 
 # Make the dbi connection info MATCH YOUR ENVIRONMENT.
+# This could set connection using the local .conf
 our $schema =
   MojoMojo::Schema->connect( 'dbi:mysql:database=mojomojo;host=localhost',
     'mojomojo', 'pass' );
 
-our $preference_rs     = $schema->resultset('Preference');
-our $page_rs           = $schema->resultset('Page');
-our $root_page         = $page_rs->find( { id => 1 } );
-our @root_lower_family = $root_page->descendants;
+our $preference_rs            = $schema->resultset('Preference');
+our $page_rs                  = $schema->resultset('Page');
+our $root_page                = $page_rs->find( { id => 1 } );
+our @root_lower_family        = $root_page->descendants;
 our $number_of_family_members = scalar @root_lower_family;
+
+# Get the howtos page for Catalyst wiki, id=113.
+# For Testing only.
+our $howtos_node = $page_rs->find( { id => 113 } );
 
 __PACKAGE__->export_to_level(1);
 
