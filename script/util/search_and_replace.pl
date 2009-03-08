6b4347cd4ddb3f140161287f3eade58e38c3e78c
mateu x hunter <hunter@missoula.org>
2009-03-08 16:36:49 -0400

    baby search and replace example.

--- script/util/search_and_replace.pl
+++ script/util/search_and_replace.pl
@@ -7,42 +7,62 @@ use MojoMojo;
 use MojoMojo::Schema;
 use Data::Dump::Streamer;
 
-my $schema = schema_connect();
-my $preference_rs            = $schema->resultset('Preference');
-print "count: ", $preference_rs->count, "\n";
-my $page_rs                  = $schema->resultset('Page');
-my $root_page                = $page_rs->find( { id => 1 } );
-my $howto_page               = $page_rs->find( { id => 113 } );
-print $howto_page->content_version, " is the content version.\n";
-my @root_lower_family        = $root_page->descendants;
-my $number_of_family_members = scalar @root_lower_family;
-print "Number of nodes: $number_of_family_members\n";
+my $schema  = schema_connect();
+my $page_rs = $schema->resultset('Page');
+
+my $search_string      = 'Catalyst';
+my $replacement_string = 'Dogalyst';
 
 while ( my $page = $page_rs->next ) {
+
     # This is for the lastest content version of a page.
-    my $page_content = $schema->resultset('Content')->search({
-        page => $page->id,
-        version => $page->content_version,
-    });
+    my $page_content = $schema->resultset('Content')->search(
+        {
+            page    => $page->id,
+            version => $page->content_version,
+        }
+    );
     if ( $page_content->count == 1 ) {
         my $content = $page_content->first->body;
-        print "content: $content\n";   
+
+        # Search on something
+        if ( $content =~ m{$search_string} ) {
+            my $number_of_replaments =
+              $content =~ s{$search_string}{$replacement_string}mxg;
+            print "Made $number_of_replaments replacments in page: ",
+              $page->name_orig, " .\n";
+
+            #print "content: $content\n";
+            # Give the adulterated content back to db.
+            $page_content->first->update( { body => $content } );
+
+            # Reverse the replacment for sanity check.
+            $content =~ s{$replacement_string}{$search_string}mxg;
+            print "Made $number_of_replaments replacments in page: ",
+              $page->name_orig, " .\n";
+            $page_content->first->update( { body => $content } );
+        }
+    }
+    elsif ( $page_content->count > 1 ) {
+        print
+          "ERROR: Seems there is more than one 'latest content version' of 
+        the page: ", $page->name, "\n";
     }
     else {
-        print "ERROR: Seems there is more than one 'latest version' of 
-        the page: ", $page->name, "\n";
+
+        # This case happens when a page is only in prototype form.  In other
+        # words, it is a node with children that has never received content
+        # of it's own.
+        print "NOTICE: Page ", $page->name,
+          " is only a prototype and therefore has no content.\n";
     }
 }
 
-
 sub schema_connect {
-    my @db_cfg = @{MojoMojo->config()->{'Model::DBIC'}->{'connect_info'}};
+    my @db_cfg = @{ MojoMojo->config()->{'Model::DBIC'}->{'connect_info'} };
 
     return MojoMojo::Schema->connect( @db_cfg[qw/0 1 2/] );
 }
-
-
-
 
 __END__
 
